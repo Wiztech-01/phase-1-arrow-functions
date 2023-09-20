e9c702c4ae98a1834d6927512661ab1d6b2cbceb
Wiztech-01 <lynnnyaoke@gmail.com>
2023-09-20 04:54:29 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+// Divide
+const divide = function(num1, num2) {
+    num1 /  num2
+    return 200 / 10
+   }
+ // Square
+   const square = (num) => num * num;
+   
+ // Add
+   const add = (num1, num2) => 3 + 4;
+ 
